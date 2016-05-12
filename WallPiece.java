5370b7c9322b6066d00e9096d40842abaeb7f050
Rhagos <daniel.wu.14@gmail.com>
2016-05-12 11:01:55 -0700

    Update and rename WallPiece to WallPiece.java

---
+++ WallPiece.java
@@ -0,0 +1,11 @@
+public class WallPiece{
+  public static final double MAX_HP = 10000;
+  private double hp;
+  public WallPiece()
+  {
+    hp = MAX_HP;
+  }
+  public double getHP(){
+    return hp;
+  }
+}
