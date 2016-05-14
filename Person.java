02a674c8cd77d2193a48e044dc116e331696939e
Rhagos <daniel.wu.14@gmail.com>
2016-05-14 12:31:06 -0700

    Update Person.java

--- Person.java
+++ Person.java
@@ -5,12 +5,14 @@ public class Person
   private double locY;
   private ArrayList<String> taskQueue;
   private double HP;
+  private double hiringPrice;
   
   public Person(double x, double y)
   {
     locX = x;
     locY = y;
-    taskQueue = new ArrayList<String>();
+    taskQueue = new ArrayList<String>();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+    hiringPrice = 200;
   }
   public void move(double dir, double distance)
   {
@@ -26,7 +28,9 @@ public class Person
   {
     for(String s:taskQueue)
     {
-      
+    	if(s.equals("")){
+    		addQueue(s);
+    	}
     }
   }
   public double getHP(){
@@ -34,6 +38,9 @@ public class Person
   }
   public void setHP(double health){
     HP = health;
+  }
+  public double getPrice(){
+	  return hiringPrice;
   }
   
 }
