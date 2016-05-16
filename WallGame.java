1fc282e12812a1bb40a28583896f0e9d53bd38b3
Rhagos <daniel.wu.14@gmail.com>
2016-05-15 23:34:29 -0700

    Update WallGame.java

--- WallGame.java
+++ WallGame.java
@@ -1,3 +1,4 @@
+import javax.swing.JFrame;
 public class WallGame
 {
   public static void main(String[]  args)
@@ -9,13 +10,17 @@ public class WallGame
     	Alien zerg = new Alien();
     	Worker worker = new Worker();
 	JFrame window = new JFrame();
-	window.setSize(500, 500);
-		
-	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
 	drawing.addPerson(wall);
 	drawing.addPerson(gaurd);
 	drawing.addPerson(worker);
 	drawing.addAlien(zerg);
+	drawing.init();
+	
+	JFrame window = new JFrame();
+	window.setSize(500, 500);
+	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	window.add(drawing);
 	
 	window.setVisible(true);
   
