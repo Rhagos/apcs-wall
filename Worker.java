0da6a707679187c97988ca01e0d8dd8e46de8873
zemobzbozz <anthonyma.wii@gmail.com>
2016-05-15 23:01:01 -0700

    Update Worker.java

--- Worker.java
+++ Worker.java
@@ -1,7 +1,14 @@
+import processing.core.PApplet;
 public class Worker extends Person{
 
   public Worker(double x, double y)
   {
     super(x,y);
+  }
+  public void draw(PApplet drawer, float x, float y)
+  {
+    super.draw(drawer,x,y);
+    drawer.fill(0);
+    drawer.ellipse(x-10,y-10,20,20);
   }
 }
