dffa0dad8ce7a4901709becbc1ab1365b5a2f8a4
zemobzbozz <anthonyma.wii@gmail.com>
2016-05-15 23:44:17 -0700

    Update WallGame.java

--- WallGame.java
+++ WallGame.java
@@ -1,21 +1,27 @@
 import javax.swing.JFrame;
+/** 
+ *  Main class that is called in order to run the entire program
+ * */
 public class WallGame
 {
+
+/** Initializes DrawingSurface and the window the program takes place in
+ * */
   public static void main(String[]  args)
   {
   	DrawingSurface drawing = new DrawingSurface();
   	
-	Wall wall = new Wall();
-	Gaurd gaurd = new Gaurd();
-    	Alien zerg = new Alien();
-    	Worker worker = new Worker();
-	JFrame window = new JFrame();
+//	Wall wall = new Wall();
+//	Gaurd gaurd = new Gaurd();
+//    	Alien zerg = new Alien();
+//    	Worker worker = new Worker();
+//	JFrame window = new JFrame();
 	
-	drawing.addPerson(wall);
-	drawing.addPerson(gaurd);
-	drawing.addPerson(worker);
-	drawing.addEnemy(zerg);
-	drawing.init();
+//	drawing.addPerson(wall);
+//	drawing.addPerson(gaurd);
+//	drawing.addPerson(worker);
+//	drawing.addEnemy(zerg);
+//	drawing.init();
 	
 	JFrame window = new JFrame();
 	window.setSize(500, 500);
