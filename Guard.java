eb2e94e3241b1b268655f501f58268d0384525ae
zemobzbozz <anthonyma.wii@gmail.com>
2016-05-15 23:20:25 -0700

    Update Guard.java

--- Guard.java
+++ Guard.java
@@ -1,14 +1,30 @@
 import processing.core.PApplet;
+/** 
+ *  This class represents a guard who is a typer Person and thus has all of Person qualities
+ *   @author Daniel Wu and Anthony Ma
+ *   @version 5/15/16
+ * */
 public class Guard extends Person{
 
+/**
+ *  Createss a Guard with default parameters at x and y 
+ *	@param x x-coordinate of the guard
+ * 	@param y y-coordinate of the guard
+ * */
   public Guard(double x, double y){
     super(x,y);
   }
-  
+/**
+ *  Checks to see if an alien is within a set range of the Guard object 
+ * */
   public boolean inRange()
   {
-    
+    return false;
   }
+ /**	Draws a new instance of a Person object with the origin set at x,y 
+  * 	@param drawer the PApplet used to draw the Person
+  *    @pre drawer must not be null and appropiate settings should already be initialized (color, fill,etc)
+  **/
   public void draw(PApplet drawer, float x, float y){
     super.draw(drawer,x,y);
     drawer.fill(0);
