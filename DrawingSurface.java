44f561d2957cf7a69c56db9a6fc758a8bf8b35ab
Rhagos <daniel.wu.14@gmail.com>
2016-05-15 21:10:56 -0700

    Update DrawingSurface.java

--- DrawingSurface.java
+++ DrawingSurface.java
@@ -4,12 +4,17 @@ import processing.core.PApplet;
 
 public class DrawingSurface extends PApplet{
 	ArrayList<Person> residents;
+	ArrayList<Alien> enemies;
 	
 	public DrawingSurface(){
 		residents = new ArrayList<Person>();
+		enemies = new ArrayList<Alien>();
 	}
 	public void addPerson(Person p){
-	  residents.add(p);
+	  	residents.add(p);
+	}
+	public void addEnemy(Alien a){
+		enemies.add(a);
 	}
 	public void setup(){
 		
@@ -19,6 +24,16 @@ public class DrawingSurface extends PApplet{
 		float x = 50;
 		float y = 50;
 		for(Person p:residents){
+			p.draw(this, x, y);
+			x += 50;
+			if(x > 500){
+				x = 50;
+				y+= 100;
+			}
+		}
+		float xE = 50;
+		float xY = y + 100;
+		for(Alien p:residents){
 			p.draw(this, x, y);
 			x += 50;
 			if(x > 500){
