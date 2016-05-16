3acbc9d621540aad1d3b0a43c13328b5aadff809
zemobzbozz <anthonyma.wii@gmail.com>
2016-05-15 23:16:40 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
-
+/**
+ *  This class represents the Player or the person playing the game and has controll over all Person objects as 
+ *  denoted by Player consisting of an Arraylist of said Person objects
+ * */
 public class Player
 {
   private ArrayList<Person> hired = new ArrayList<Person>();
