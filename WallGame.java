13fc4540102fc24a83c97555951796e7c2397d21
zemobzbozz <anthonyma.wii@gmail.com>
2016-05-15 17:51:04 -0700

    Update WallGame.java

--- WallGame.java
+++ WallGame.java
@@ -2,6 +2,17 @@ public class WallGame
 {
   public static void main(String[]  args)
   {
+    Wall marker = new Wall();
+    Person dude = new Person();
+		marker.init();
+		person.init();
+		JFrame window = new JFrame();
+		window.setSize(500, 500);
+		
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.add(marker);
+		window.add(dude);
+		window.setVisible(true);
   
   }
 }
