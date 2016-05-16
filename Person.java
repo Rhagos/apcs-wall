3558e541ea182c0582b75dd4ceb5e5cff187ff75
zemobzbozz <anthonyma.wii@gmail.com>
2016-05-15 22:35:08 -0700

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,12 +1,14 @@
 import java.util.ArrayList;
+
 import processing.core.PApplet;
 public class Person
 {
-  private double locX;
-  private double locY;
+  
   private ArrayList<String> taskQueue;
   private double HP;
   private double hiringPrice;
+  private double locX;
+  private double locY;
   private int defaultX;
   private int defaultY;
   private int defaultW;
@@ -47,10 +49,11 @@ public class Person
   public double getPrice(){
 	  return hiringPrice;
   }
-   public void draw(PApplet drawer)
+   public void draw(PApplet drawer, float x, float y)
    {
 	 drawer.stroke(0);
-
+	 locX = x; 
+	 locY = y; 
 		// float yRatio = drawer.height/500f; // float can be either cast via (float) or f behind the integer
 		// float xRatio = drawer.width/500f; // java sees 500 as an automatic int type
 		// drawer.scale(xRatio,yRatio)
