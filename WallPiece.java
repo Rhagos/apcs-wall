097240d0ba158adb8718526527597f843c572b1d
Rhagos <daniel.wu.14@gmail.com>
2016-05-15 23:23:57 -0700

    Update WallPiece.java

--- WallPiece.java
+++ WallPiece.java
@@ -1,3 +1,6 @@
+
+import java.awt.Color;
+
 public class WallPiece{
   public static final double MAX_HP = 10000;
   private double hp;
@@ -7,5 +10,10 @@ public class WallPiece{
   }
   public double getHP(){
     return hp;
+  }
+  public void draw(PApplet drawer, float x, float y){
+    drawer.line(Color.RED.getRGB());
+    drawer.fill(Color.LIGHT_GRAY.getRGB());
+    drawer.rect(x,y,50,50);
   }
 }
