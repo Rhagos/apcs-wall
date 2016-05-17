e35a3e66f49344f2f256aee717968a4cdd0d2a46
zemobzbozz <anthonyma.wii@gmail.com>
2016-05-17 08:40:48 -0700

    Update WallGame.java

--- WallGame.java
+++ WallGame.java
@@ -1,4 +1,10 @@
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
 /** 
  *  Main class that is called in order to run the entire program
  * */
@@ -7,8 +13,18 @@ public class WallGame
 
 /** Initializes DrawingSurface and the window the program takes place in
  * */
+ 
+  public JMenuBar setUp() throws Exception{
+  	AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File("MenuMusic.mp3"));
+  	Clip clip = AudioSystem.getClip();
+  	clip.open(inputStream);
+  	clip.loop(clip.LOOP_CONTINUOUSLY);
+	return null;
+  	
+  }
   public static void main(String[]  args)
   {
+ 
   	DrawingSurface drawing = new DrawingSurface();
   	
 	Wall wall = new Wall();
