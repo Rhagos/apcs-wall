c1ee3ac07f6b70691be7f190d23a2cce6d0f3af8
Rhagos <daniel.wu.14@gmail.com>
2016-05-19 00:06:35 -0700

    Update Worker.java

--- src/Worker.java
+++ src/Worker.java
@@ -9,7 +9,7 @@ import processing.core.PApplet;
 public class Worker extends Person{
 
 /**
- *  Createss a Worker with default parameters at x and y 
+ *  Creates a Worker with default parameters at x and y 
  *	@param x x-coordinate of the worker
  * 	@param y y-coordinate of the worker
  * */
@@ -17,14 +17,34 @@ public class Worker extends Person{
   {
     super(x,y);
   }
+  
+  /**
+   * Repairs the target wall piece
+   * @param w WallPiece that is to be repaired
+   * @pre w is damaged and not null
+   */
+  public void repair(WallPiece w){
+	  w.healWall(getLevel() * 10);
+	  addXP(10);
+  }
+  /**
+   * Moves the worker towards the wall
+   * @param p Player object containing the other objects
+   */
+  public void act(Player p){
+	  Wall targetWall = p.getWall();
+	  WallPiece target = targetWall.getDamaged();
+	  move(-1* directionTo(new Person(0,200)), 5);
+  }
+  
  /**	Draws a new instance of a Person object with the origin set at x,y 
   * 	@param drawer the PApplet used to draw the Person
-  *    @pre drawer must not be null and appropiate settings should already be initialized (color, fill,etc)
+  *    @pre drawer must not be null and appropriate settings should already be initialized (color, fill,etc)
   **/
-  public void draw(PApplet drawer, float x, float y)
+  public void draw(PApplet drawer)
   {
-    super.draw(drawer,x,y);
+    super.draw(drawer);
     drawer.fill(Color.YELLOW.getRGB());
-    drawer.ellipse(x-10,y-10,20,20);
+    drawer.ellipse((float)getX()-10,(float)getY()-10,20,20);
   }
 }
