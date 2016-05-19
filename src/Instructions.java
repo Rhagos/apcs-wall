6e53933d34e35425f50c8ac2de45868daf4351ca
zemobzbozz <anthonyma.wii@gmail.com>
2016-05-18 20:54:11 -0700

    Create Instructions.java

---
+++ src/Instructions.java
@@ -0,0 +1,26 @@
+import javax.swing.JFrame;
+
+import processing.core.PApplet;
+
+public class Instructions extends PApplet {
+
+	public void draw(){
+		JFrame window = new JFrame();
+		background(255);
+		fill(255);
+		//rect(100,300,300,30,7);
+		rect(100,200,300,300,7);
+		
+		fill(0);
+		textAlign(LEFT);
+		text("Instructions",220,220);
+		textAlign(CENTER);
+		text("    The workers and defenders will routinely perform \n"+
+		"their tasks autonomously and all allied units \n"+ "and structures are upgradable for various benefits ",250,250);
+		
+	
+		window.setSize(500, 500);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+}
