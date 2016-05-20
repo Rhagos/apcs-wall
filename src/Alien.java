98139e05dece5fc5ff3be81cc8f356aaeeba0307
Rhagos <daniel.wu.14@gmail.com>
2016-05-19 20:32:03 -0700

    Update Alien.java

--- src/Alien.java
+++ src/Alien.java
@@ -30,16 +30,45 @@ public class Alien extends Person{
 	    locX = x;
 	    locY= y;
 	  }
+	  
+	  /**
+	   * Moves the alien towards the wall
+	   * @param p Player object containing the other objects
+	   */
+	  public void act(Player p){
+		  Wall targetWall = p.getWall();
+		  WallPiece target = getClosest(targetWall);
+		  if(target != null){
+			  if(distanceTo(new Person(target.getX(),target.getY())) < 60){
+				  target.damageWall(10);
+			  }else{
+				  move(-1 *directionTo(new Person(target.getX(),target.getY())), 2);
+			  }
+		  }
+	  }
+	  
+	  public WallPiece getClosest(Wall w){
+		  double maxDist = Integer.MAX_VALUE;
+		  WallPiece target = new WallPiece(999,999,999);
+		  for(int i = 0; i < w.getWallParts().size(); i++){
+			  WallPiece temp = w.getWallParts().get(i);
+			  if(temp != null)
+			  if(distanceTo(new Person(temp.getX(),temp.getY())) < maxDist){
+				  maxDist = distanceTo(new Person(target.getX(),target.getY()));
+				  target = w.getWallParts().get(i);
+			  }
+		  }
+		  return target;
+	  }
+	  
    /**	Draws a new instance of a Alien object with the origin set at x,y 
    * 	@param drawer the PApplet used to draw the Alien
    *    @pre drawer must not be null and appropiate settings should already be initialized (color, fill,etc)
    * */
-	public void draw(PApplet drawer, float x, float y)
+	public void draw(PApplet drawer)
 	{
 		drawer.stroke(0);
 		drawer.fill(Color.ORANGE.getRGB());
-		 locX = x; 
-		 locY = y;
-		 drawer.ellipse(x,y,20,30); 
+		 drawer.ellipse((float)getX(),(float)getY(),20,30); 
 		}
 }
