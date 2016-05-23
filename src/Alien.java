c042112c56d601d0ec66cea64e7c36532a961233
Rhagos <daniel.wu.14@gmail.com>
2016-05-22 18:58:44 -0700

    Update Alien.java

--- src/Alien.java
+++ src/Alien.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.image.ImageObserver;
 import java.util.ArrayList;
 
@@ -17,28 +18,34 @@ import processing.core.PApplet;
 public class Alien extends Entity{
 	/**
 	 * 	Creates a default instance of Alien with a given position and set dimensions of 30x20
+	 * 	@param string filename of the image
 	 * 	@param x x-coordinate of the alien
 	 * 	@param y y-coordinate of the alien
 	 * */
-	  public Alien(double x, double y)
-	  {
-		  super("Dungeon Floor Updated/Wall_1.png", (int) x, (int) y, 30, 20);
-	    
+	  public Alien(String string, double x, double y) {
+		  super(string,(int)x,(int)y,30,20);
 	  }
-	  
-	  /**
+
+	  public Image getImage()
+	  {
+		  return new ImageIcon("AlienSprite/Alien_1.png").getImage();
+		  
+	  }
+	/**
 	   * Moves the alien towards the wall
 	   * @param p Player object containing the other objects
 	   */
 	  public void act(Player p){
 		  super.act(p);
-		  Wall targetWall = p.getWall();
-		  WallPiece target = getClosest(targetWall);
-		  if(target != null){
-			  if(distanceTo(new Person(null,target.getX()+25,target.getY()+25)) < 60){
-				  target.damageWall(10);
-			  }else{
-				  move((int)(-1 * directionTo(new Person(null,target.getX()+25,target.getY()+25))), 2);
+		  if(!getLifeStatus()){
+			  Wall targetWall = p.getWall();
+			  WallPiece target = getClosest(targetWall);
+			  if(target != null){
+				  if(distanceTo(new Person(null,target.getX()+25,target.getY()+25)) < 60){
+					  target.damageWall(10);
+				  }else{
+					  move((int)(directionTo(new Person(null,target.getX()+25,target.getY()+25))), 2);
+				  }
 			  }
 		  }
 	  }
@@ -56,9 +63,5 @@ public class Alien extends Entity{
 		  }
 		  return target;
 	  }
-	   public Image getImage()
-	  {
-		  return new ImageIcon("AlienSprite/Alien_1.png").getImage();
-		  
-	  }
+	  
 }
