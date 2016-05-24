c268b8aa161b65092d99a98c5982a71743508d5f
zemobzbozz <anthonyma.wii@gmail.com>
2016-05-23 23:53:25 -0700

    Update Turret.java

--- src/Turret.java
+++ src/Turret.java
@@ -2,13 +2,29 @@ import java.awt.Color;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
-
+/**
+ * This class represents a turret of type WallPiece 
+ * 
+ * @author Daniel Wu and Anthony Ma
+ * @version 5/15/16
+ * 
+ **/
 public class Turret extends WallPiece{
-
+	/**
+	 *   Constructs a turret object with a changable X,Y Coords, Piecenum, 
+	 *   @param xCoord sets the turret at this xCoord
+	 *   @param yCoord sets the turret at this yCoord 
+	 *   @param pieceNam onto which piece it is on 
+	 *   @param w what number wall it is 
+	 * */
 	public Turret(double x, double y, int pieceNum, Wall w) {
 		super(x, y, pieceNum, w);
 		// TODO Auto-generated constructor stub
 	}
+	/**
+	 *  Shooots at aliens
+	 *  @param p type player objects 
+	 * */
 	public void act(Player p){
 		super.act(p);
 		for(int i = 0; i < p.getEnemy().size(); i++){
@@ -18,6 +34,10 @@ public class Turret extends WallPiece{
 			}
 		}
 	}
+	
+	/**
+	 *  Returns image of Turret
+	 * */
 	public Image getImage(){
 		return new ImageIcon("Dungeon Floor Updated/Wall_9.png").getImage();
 	}
