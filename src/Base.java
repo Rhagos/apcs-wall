ee9afbccf290a0fc5b156c81b54dbdb0849f3fff
zemobzbozz <anthonyma.wii@gmail.com>
2016-05-23 23:14:04 -0700

    Update Base.java

--- src/Base.java
+++ src/Base.java
@@ -1,17 +1,23 @@
-
+/**
+ *  Creates a Base object of type Building which spawns Person related objects 
+ *  
+ * */
 public class Base extends Building{
 	
 	private int timer;
 	private static final int FIRE_TIME = 20;
-	
+	/**
+	 *   Constructs a base object with a set image and set health 
+	 * */
 	public Base(){
 		super("PersonSpawnSprite/PersonSpawn_1.png",500,75,75,75);
 		setHP(100000);
 	}
-	
+	/**
+	 *  Base shoots nearby enemies if they get too close 
+	 * */
 	public void act(Player p){
 		super.act(p);
-		System.out.println("BASE");
 		timer++;
 		for(Alien a: p.getEnemy()){
 			if(distanceTo(a) < 150 && timer >= FIRE_TIME){
