93f1780b6fd4362cd35f4c10b9e396bb71de0525
Rhagos <daniel.wu.14@gmail.com>
2016-05-23 23:55:30 -0700

    Update WallRunnerScreen.java

--- src/WallRunnerScreen.java
+++ src/WallRunnerScreen.java
@@ -5,28 +5,31 @@ import java.awt.event.*;
 
 
 public class WallRunnerScreen extends JPanel implements ActionListener {
-	
 	WallMain w;
-	
+	JButton button1;
+	JButton button3;
 	public WallRunnerScreen(WallMain w) {
 		this.w = w;
 	
-		JButton button1 = new JButton("Play");
+		button1 = new JButton("Play");
 		button1.addActionListener(this);
 		button1.setAlignmentX(Component.CENTER_ALIGNMENT);
+		button1.setAlignmentY(Component.CENTER_ALIGNMENT);
 		add(button1);
-		JButton button2 = new JButton("Settings");
-		button2.addActionListener(this);
-		button2.setAlignmentX(Component.CENTER_ALIGNMENT);
-		add(button2);
-		JButton button3 = new JButton("Instructions");
+		button3 = new JButton("Instructions");
 		button3.addActionListener(this);
 		button3.setAlignmentX(Component.CENTER_ALIGNMENT);
+		button1.setAlignmentY(Component.CENTER_ALIGNMENT);
 		add(button3);
 		
 	}
 	
 	public void actionPerformed(ActionEvent e) {
-		w.changePanel();
+		if(e.getSource().equals(button1)){
+			w.changePanel();
+		}
+		if(e.getSource().equals(button3)){
+			w.changeToHelp();
+		}
 	}
 } 
