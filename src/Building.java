abd88e29a0b41bcc65e6d69f053747d6e497d283
zemobzbozz <anthonyma.wii@gmail.com>
2016-05-23 23:28:54 -0700

    Update Building.java

--- src/Building.java
+++ src/Building.java
@@ -10,6 +10,11 @@ public class Building extends Entity{
 
 	/**
 	 * 	Constructs a building with a default image, XCoord, YCoord, width, and height
+	 *  @param image sets the image 
+	 *  @param x sets the XCoord
+	 *  @param y sets the YCoord
+	 *  @param w sets the width
+	 *  @param h sets the height
 	 * */
 	public Building(String image, int x, int y, int w, int h) {
 		super(image, x, y, w, h);
